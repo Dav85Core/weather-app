ab79b0f60ea4715319123f33880f2ab0969499ab
DAVIDE CORDA <AMAZINGDAVE85@gmail.com>
2023-01-08 11:02:45 +0100

    test MUI  button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi!</h1>
+      <Button variant="contained">Hello World</Button>
+      
     </div>
   );
 }
